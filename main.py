f86014694cb35500a9d7d3dfe7af85e5decd4c25
Kusuma <kusumo999666@gmail.com>
2023-08-02 22:26:42 +0700

    scrape various website

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://id.carousell.com/categories/computers-and-tablets-333'
+url = 'https://www.realtor.com/realestateandhomes-search/Los-Angeles_CA/show-recently-sold/sby-6'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 '
@@ -13,11 +13,26 @@ res = requests.get(url, headers=headers)
 
 def get_total_pages():
     soup = BeautifulSoup(res.text, 'html.parser')
-    laptop_best_match = soup.find(attrs={'class': 'D_Z asm-browse-listings'})
-    titles = laptop_best_match.findAll(attrs={'class': 'D_sZ D_BK'})
-    for title in titles:
-        print(title.find('p', attrs={
-            'class': 'D_qz M_kQ D_pl M_kE D_q_ M_kR D_qE M_kV D_qH M_kY D_qK M_lb D_qM M_le D_qI M_kZ D_qQ'}).text)
+    recently_sold_page = soup.find(attrs={'class': 'PropertiesList_propertiesContainer__j6ct_ '
+                                                   'PropertiesList_listViewGrid__oGuSL'})
+    items = recently_sold_page.findAll(attrs={'class': 'BasePropertyCard_propertyCardWrap__J0xUj'})
+
+    contents = []
+    for item in items:
+        try:
+            broker = item.find('span', 'BrokerTitle_titleText__20u1P').text
+        except:
+            broker = 'Broker is not available'
+        price = item.find('div', 'Price__Component-rui__x3geed-0 ibnFqH card-price').text
+        adrress = item.find('div', 'card-address truncate-line').text
+
+        data_dict = {
+            'broker': broker,
+            'price': price,
+            'adress': adrress
+        }
+        contents.append(data_dict)
+    print(f'Count Data: {len(contents)}')
 
 
 if __name__ == '__main__':
