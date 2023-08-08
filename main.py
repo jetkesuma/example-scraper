d226d8b9d45cd2a195745b86f94bb48ec4193e74
Kusuma <kusumo999666@gmail.com>
2023-08-08 19:51:14 +0700

    almost...

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 loc = 'California'
-page = 'pg-2'
-url = 'https://www.realtor.com/realestateandhomes-search/{}/{}'.format(loc, page)
+url = 'https://www.realtor.com/realestateandhomes-search/{}/pg-2'.format(loc)
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 '
@@ -20,6 +19,20 @@ def get_total_pages():
     items = soup.findAll('div', 'BasePropertyCard_propertyCardWrap__J0xUj')
     for item in items:
         broker = item.find('span', 'BrokerTitle_titleText__20u1P').text
+        status = item.find('div', 'Text__StyledText-rui__sc-1j9ntoo-0 hkRBUb TypeInfo__StyledInfo-rui__m9gzjc-0 '
+                                  'eVFIjb message').text
+        price = item.find('div', 'Price__Component-rui__x3geed-0 ibnFqH card-price').text
+        try :
+            wide = item.find('span', {'data-testid': 'screen-reader-value'}).text
+        except :
+            wide = 'No Value'
+        loc = item.find('div', 'card-address truncate-line').text
+        try:
+            img = item.find('div', 'Picture_photo-wrap__xFx0Q').find('img')['src']
+        except:
+            img = 'No Image'
+        print(img)
+
 
 
 if __name__ == '__main__':
